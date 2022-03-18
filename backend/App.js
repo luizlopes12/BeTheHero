e6272d1cb4e1f3b03856c1620ff5acbbb0d6aca9
Luiz Lopes <luizlopes12@outlook.com>
2022-03-18 11:12:25 -0300

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const App = express()
 const cors = require('cors')
+const port = 3000
+
+App.use(cors())
+App.use(express.json())
+
+App.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    console.log('Funcionando')
+})
