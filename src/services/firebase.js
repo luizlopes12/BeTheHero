ea3c14761ea5e24314a52185af02b1c1eaa3cedf
Luiz Lopes <luizlopes12@outlook.com>
2022-03-24 15:34:02 -0300

    Configs iniciais firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+
+import 'firebase/compat/auth'
+import 'firebase/compat/database'
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database();
+
+export {firebase, auth, database}
