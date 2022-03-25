46bb8ed6d6b26990ed5c8bd3afb2aa21d395b7c4
Luiz Lopes <luizlopes12@outlook.com>
2022-03-25 13:49:19 -0300

    Estilização global básica

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react'
-import { database } from './services/firebase'
-console.log(database.ref('cases'))
+import GlobalStyles from './global/GlobalStyles'
+
+
+
+
+// import { database } from './services/firebase'
+
+
+// const testGetDb = () => {
+//   database.ref('cases').on('value', item =>{
+//   console.log(item.val())
+// })}
+// const testInsertDb = () =>{
+//   database.ref('cases').push({
+//     id: 2,
+//     title:'Titulo 2',
+//     description: 'Descrição 2',
+//     price: '22',
+//     contact: {
+//       ongEmail: 'luiz@luiz.com',
+//       ongWhatsapp: '999999999'
+//     }
+//   })
+// }
 const App = () => {
   return (
+    <>
+    <GlobalStyles/>
     <div>
-      App
+      OPA
     </div>
+    </>
   )
 }
 
---
+++ src/global/GlobalStyles/index.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+const primary = '#F0F0F5'
+
+
+const GlobalStyle = createGlobalStyle`
+    *,body{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+        font-family: 'Roboto', sans-serif;
+    }
+
+    body{
+        background: ${primary};
+    }
+`;
+
+
+export default GlobalStyle
