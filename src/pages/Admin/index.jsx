d864dd22c5538ef77c2dbd1b739f163cdf4ac14f
Luiz Lopes <luizlopes12@outlook.com>
2022-03-28 14:54:51 -0300

    Update index.jsx

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 const Admin = () => {
-    const ongState = useSelector(state=> state.userData)
+    const ongData = useSelector(state=> state.userData)
     const navigate = useNavigate()
-    const [ongData, setOngData] = useState(ongState.length > 0 ? ongState: false)
     useEffect(()=>{
       !ongData && navigate('/')
     },[])
