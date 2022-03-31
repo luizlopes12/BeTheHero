84caba72a980082f526d99ad350a246faef6f2a7
Luiz Lopes <luizlopes12@outlook.com>
2022-03-31 15:54:29 -0300

    Update index.jsx

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -3,8 +3,7 @@ import { useSelector } from 'react-redux'
 import AdminHeader from '../../components/AdminHeader'
 import { AdminStyle } from './styled'
 const Admin = () => {
-    const ongData = useSelector(state=> state.userData)
-    const name = ongData[1].ongName
+  
   return (
       <AdminStyle>
         <AdminHeader/>
