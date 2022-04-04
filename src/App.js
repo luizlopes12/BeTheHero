ef843e9105e0df128aca8caec0579ef9a4f10a5d
Luiz Lopes <luizlopes12@outlook.com>
2022-04-03 22:52:15 -0300

    Deploy no firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import GlobalStyles from "./global/GlobalStyles";
 import { Provider } from "react-redux";
-import Footer from "./components/Footer";
 import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesManager from "./RoutesManager";
@@ -12,7 +11,6 @@ const App = () => {
         <GlobalStyles />
         <RoutesManager />
       </Router>
-      <Footer />
     </Provider>
   );
 };
